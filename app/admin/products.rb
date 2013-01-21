89f30d09f6bb290ca29ee945b20ac6238ea10634
Renat Darybaev <renatd.k@gmail.com>
2013-01-21 19:53:37 +0600

    adds

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,11 +1,12 @@
+# coding: utf-8
 ActiveAdmin.register Product do
 
   form do |f|
     f.inputs do
-      f.input :title
-      f.input :new
-      f.input :spec
-      f.input :yes
+      f.input :title, as: :check_boxes
+      f.input :new, as: :check_boxes
+      f.input :spec, as: :check_boxes
+      f.input :yes, as: :check_boxes
       f.input :norm
       f.input :subcatalogs, as: :check_boxes
       f.input :name
@@ -18,5 +19,14 @@ ActiveAdmin.register Product do
     f.buttons
   end
 
+  index do
+    column :id
+
+    column "Наименование" do |product|
+      link_to product.name, admin_product_path(product)
+    end
+    column "На главной", :title
+    default_actions
+  end
 
 end
