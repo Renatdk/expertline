32a2dfc1b25fc35a873a63e7beecd0a9fd966063
Renat Darybayev <renatd.k@gmail.com>
2014-07-19 09:59:44 +0600

    ddd

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,7 +1,7 @@
 class Sender < ActiveRecord::Base
   attr_accessible :address, :basket_id, :buy, :commet, :email, :product_id, :sicret, :tel, :company, :name, :docs
 
-  validates :email,:name, :tel,:address => true
+  validates :email,:name, :tel,:address, :presence => true
 
 
   belongs_to :basket
