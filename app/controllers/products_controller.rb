edd5bc4cedca67322f8a9b93af8bf2f2e6ac80b1
Renat Darybayev <renatd.k@gmail.com>
2014-09-11 09:55:19 +0600

    add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,12 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
-
+    
     respond_to do |format|
       format.html
       format.csv { send_data @products.to_csv }
       format.xls # { send_data @products.to_csv(col_sep: "\t") }
-      format.json { render json: @products }
+      format.json{render :json => @products.to_json, :callback => params['callback']}
     end
   end
 
